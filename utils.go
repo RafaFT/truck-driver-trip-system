a62e6ff98dd81c5b21873b95edf9c9dbb253ed4d
Rafael FT <rafael.ftrad@gmail.com>
2020-07-04 13:38:15 -0300

    feat: create function for constructing firestore Query by the URL's query string.

---
+++ utils.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"net/http"
+	"strconv"
+	"strings"
+
+	"cloud.google.com/go/firestore"
+	"github.com/gorilla/mux"
+)
+
+func createQuery(collection *firestore.CollectionRef, r *http.Request) firestore.Query {
+	q := collection.Query
+
+	// cpf will only exists if it's the getDriver`s route
+	if rawCPF, exist := mux.Vars(r)["cpf"]; exist {
+		cpf := strings.ReplaceAll(strings.ReplaceAll(rawCPF, ".", ""), "-", "")
+		q = q.Where("cpf", "==", cpf) // TODO: query for the document ID
+	}
+	if gender := r.Form.Get("gender"); len(gender) > 0 {
+		q = q.Where("gender", "==", strings.ToUpper(gender))
+	}
+	if str_has_vehicle := r.Form.Get("has_vehicle"); len(str_has_vehicle) > 0 {
+		has_vehicle, err := strconv.ParseBool(str_has_vehicle)
+		if err == nil {
+			q = q.Where("has_vehicle", "==", has_vehicle)
+		}
+	}
+	if cnh_type := r.Form.Get("cnh_type"); len(cnh_type) > 0 {
+		q = q.Where("cnh_type", "==", strings.ToUpper(cnh_type))
+	}
+
+	// get only requested fields
+	if rawFields := r.Form.Get("fields"); len(rawFields) > 0 {
+		splitFields := strings.Split(rawFields, ",")
+		fields := make([]string, 0)
+		for _, field := range splitFields {
+			if len(field) > 0 {
+				fields = append(fields, field)
+			}
+		}
+
+		q = q.Select(fields...)
+	}
+
+	return q
+}
