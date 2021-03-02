1698ce3ea13e3b9766801a5a54bacc065b012391
RafaFT <rafael.ftrad@gmail.com>
2021-03-02 00:58:29 -0300

    feat: add DriverPresenter interface (output port).

--- src/usecase/driver_interactor.go
+++ src/usecase/driver_interactor.go
@@ -2,12 +2,20 @@ package usecase
 
 import (
 	"context"
-	"time"
+
+	"github.com/rafaft/truck-driver-trip-system/entity"
 )
 
 // input port
 type DriverUseCase interface {
 	CreateDriver(context.Context, CreateDriverInput) (CreateDriverOutput, error)
-	DeleteDriver(context.Context, string) (time.Time, error)
+	DeleteDriver(context.Context, string) error
 	UpdateDriver(context.Context, string, UpdateDriverInput) (UpdateDriverOutput, error)
+}
+
+// output port
+type DriverPresenter interface {
+	CreateDriverOutput(*entity.Driver) CreateDriverOutput
+	DeleteDriverOutput(error) error
+	UpdateDriverOutput(*entity.Driver) UpdateDriverOutput
 }
