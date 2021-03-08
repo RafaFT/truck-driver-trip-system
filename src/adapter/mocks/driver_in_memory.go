47689e18ee2d99a1c355f6838b7f090e4b9bb485
RafaFT <rafael.ftrad@gmail.com>
2021-03-08 02:03:49 -0300

    feat: create interface adapter mock for driver repository (in memory).

---
+++ src/adapter/mocks/driver_in_memory.go
@@ -0,0 +1,64 @@
+package mocks
+
+import (
+	"context"
+
+	"github.com/rafaft/truck-driver-trip-system/entity"
+)
+
+// driver repository mock implementation
+type InMemoryDrivers struct {
+	Drivers []*entity.Driver
+}
+
+func (d InMemoryDrivers) DeleteDriverByCPF(ctx context.Context, cpf entity.CPF) error {
+	for i, driver := range d.Drivers {
+		if driver.CPF() == cpf {
+			d.Drivers[i] = d.Drivers[len(d.Drivers)-1]
+			d.Drivers = d.Drivers[:len(d.Drivers)-1]
+
+			return nil
+		}
+	}
+
+	return entity.NewErrDriverNotFound(cpf)
+}
+
+func (d InMemoryDrivers) FindDriverByCPF(ctx context.Context, cpf entity.CPF) (*entity.Driver, error) {
+	for _, driver := range d.Drivers {
+		if driver.CPF() == cpf {
+			return driver, nil
+		}
+	}
+
+	return nil, entity.NewErrDriverNotFound(cpf)
+}
+
+func (d InMemoryDrivers) FindDrivers(ctx context.Context) ([]*entity.Driver, error) {
+	return d.Drivers, nil
+}
+
+func (d InMemoryDrivers) SaveDriver(ctx context.Context, driver *entity.Driver) error {
+	for _, storedDriver := range d.Drivers {
+		if storedDriver.CPF() == driver.CPF() {
+			return entity.NewErrDriverAlreadyExists(driver.CPF())
+		}
+	}
+
+	d.Drivers = append(d.Drivers, driver)
+
+	return nil
+}
+
+func (d InMemoryDrivers) UpdateDriver(ctx context.Context, driver *entity.Driver) error {
+	for i, storedDriver := range d.Drivers {
+		if storedDriver.CPF() == driver.CPF() {
+			updatedDriver := *driver
+			d.Drivers[i] = &updatedDriver
+
+			return nil
+		}
+	}
+
+	return entity.NewErrDriverNotFound(driver.CPF())
+}
