e58f3639baecdba2da90420536cfcafa451672e3
RafaFT <rafael.ftrad@gmail.com>
2021-03-15 22:54:41 -0300

    refactor: create driver controller now has it's own input, and return error specifying missing fields.

--- src/adapter/controller/rest/create_driver.go
+++ src/adapter/controller/rest/create_driver.go
@@ -3,12 +3,58 @@ package rest
 import (
 	"encoding/json"
 	"fmt"
-	"io"
 	"net/http"
+	"strings"
+	"time"
 
 	"github.com/rafaft/truck-driver-trip-system/entity"
 	"github.com/rafaft/truck-driver-trip-system/usecase"
 )
+
+type createDriverInput struct {
+	BirthDate  *time.Time `json:"birth_date"`
+	CNH        *string    `json:"cnh"`
+	CPF        *string    `json:"cpf"`
+	Gender     *string    `json:"gender"`
+	HasVehicle *bool      `json:"has_vehicle"`
+	Name       *string    `json:"name"`
+}
+
+func (cd createDriverInput) writeUCInput(ucInput *usecase.CreateDriverInput) error {
+	missingFields := make([]string, 0, 6)
+
+	if cd.BirthDate == nil {
+		missingFields = append(missingFields, "birth_date")
+	}
+	if cd.CNH == nil {
+		missingFields = append(missingFields, "cnh")
+	}
+	if cd.CPF == nil {
+		missingFields = append(missingFields, "cpf")
+	}
+	if cd.Gender == nil {
+		missingFields = append(missingFields, "gender")
+	}
+	if cd.HasVehicle == nil {
+		missingFields = append(missingFields, "has_vehicle")
+	}
+	if cd.Name == nil {
+		missingFields = append(missingFields, "name")
+	}
+
+	if len(missingFields) > 0 {
+		return fmt.Errorf("missing fields: [%s]", strings.Join(missingFields, ", "))
+	}
+
+	ucInput.BirthDate = *cd.BirthDate
+	ucInput.CNH = *cd.CNH
+	ucInput.CPF = *cd.CPF
+	ucInput.Gender = *cd.Gender
+	ucInput.HasVehicle = *cd.HasVehicle
+	ucInput.Name = *cd.Name
+
+	return nil
+}
 
 type CreateDriverController struct {
 	url string
@@ -25,22 +71,21 @@ func NewCreateDriverController(url string, uc usecase.CreateDriverUseCase) Creat
 func (c CreateDriverController) ServerHTTP(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 
-	body, err := io.ReadAll(r.Body)
-	if err != nil {
+	var input createDriverInput
+	if err := json.NewDecoder(r.Body).Decode(&input); err != nil {
 		w.WriteHeader(http.StatusBadRequest)
 		w.Write([]byte(`{"error": "invalid request body"}`))
 		return
 	}
 
-	var input usecase.CreateDriverInput
-	err = json.Unmarshal(body, &input)
-	if err != nil {
+	var ucInput usecase.CreateDriverInput
+	if err := input.writeUCInput(&ucInput); err != nil {
 		w.WriteHeader(http.StatusBadRequest)
-		w.Write([]byte(`{"error": "invalid request body"}`))
+		w.Write([]byte(fmt.Sprintf(`{"error": "%s"}`, err.Error())))
 		return
 	}
 
-	output, err := c.uc.Execute(r.Context(), input)
+	output, err := c.uc.Execute(r.Context(), ucInput)
 	if err != nil {
 		var code int
 		var msg string
