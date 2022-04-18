dcd64ec3f69bfa2deea2ee44ed5b7b1b7d96c4cb
RafaFT <rafael.ftrad@gmail.com>
2022-04-17 21:32:28 -0300

    refactor: use interface embedding for creating Driver and Trip general repositories.

--- src/usecase/driver_repository.go
+++ src/usecase/driver_repository.go
@@ -1,15 +1,9 @@
 package usecase
 
-import (
-	"context"
-
-	"github.com/rafaft/truck-driver-trip-system/entity"
-)
-
 type DriverRepository interface {
-	DeleteByCPF(context.Context, entity.CPF) error
-	FindByCPF(context.Context, entity.CPF) (*entity.Driver, error)
-	Find(context.Context, FindDriversQuery) ([]*entity.Driver, error)
-	Save(context.Context, *entity.Driver) error
-	Update(context.Context, *entity.Driver) error
+	CreateDriverRepo
+	DeleteDriverRepo
+	GetDriverByCPFRepo
+	GetDriverRepo
+	UpdateDriverRepo
 }
