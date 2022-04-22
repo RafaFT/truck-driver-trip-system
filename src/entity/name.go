4717c95b2c20f6a2d70d79505fd41fac102fb412
RafaFT <rafael.ftrad@gmail.com>
2022-04-21 23:48:25 -0300

    perf: small reduce of used memory for valid Name checks.

--- src/entity/name.go
+++ src/entity/name.go
@@ -11,11 +11,9 @@ var spacePattern = regexp.MustCompile(`[^\S\t\v\r]`)
 type Name string
 
 func NewName(name string) (Name, error) {
-	err := NewErrInvalidName(name)
-
 	// check min and (arbitrary) max runes limit
 	if length := len([]rune(name)); length == 0 || length > 127 {
-		return "", err
+		return "", NewErrInvalidName(name)
 	}
 
 	tokens := spacePattern.Split(name, -1)
@@ -23,12 +21,12 @@ func NewName(name string) (Name, error) {
 	for _, token := range tokens {
 		// empty token signals leading/trailing or an extra space in the input name
 		if len(token) == 0 {
-			return "", err
+			return "", NewErrInvalidName(name)
 		}
 
 		for _, chr := range token {
 			if !unicode.IsLetter(chr) {
-				return "", err
+				return "", NewErrInvalidName(name)
 			}
 		}
 	}
